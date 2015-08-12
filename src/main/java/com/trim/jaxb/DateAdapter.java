4e7dea90526439bc88cb7e469aa0445d74f5f92f
giambattista <giambapisasale@insolaria.it>
2015-08-12 17:05:11 +0200

    prima release

--- src/main/java/com/trim/jaxb/DateAdapter.java
+++ src/main/java/com/trim/jaxb/DateAdapter.java
@@ -2,20 +2,18 @@ package com.trim.jaxb;
 
 import java.util.Date;
 
+/**
+ * used only for jaxb type setting on conversion
+ * @author giambattista
+ *
+ */
 public class DateAdapter {
-	// private static SimpleDateFormat df = new SimpleDateFormat("ddMMyy");
 
 	public static Date parseDate(String s) {
-		// try {
-		// return df.parse(s);
-		// } catch (ParseException e) {
-		//// log.error
-		// }
 		return null;
 	}
 
 	public static String printDate(Date dt) {
 		return null;
-		// return df.format(dt);
 	}
 }
