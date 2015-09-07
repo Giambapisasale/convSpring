4dd507e13dd2992cd6d9c1456c9e6d0c84f24dd6
giambattista <giambapisasale@insolaria.it>
2015-09-07 10:11:03 +0200

    caching template xslt

--- src/main/java/com/xmlconverter/CBI_RH_Processor2.java
+++ src/main/java/com/xmlconverter/CBI_RH_Processor2.java
@@ -1,12 +1,6 @@
 package com.xmlconverter;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamSource;
 
 import org.dom4j.Document;
 import org.dom4j.io.DocumentResult;
@@ -14,40 +8,37 @@ import org.dom4j.io.DocumentSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.beans.factory.annotation.Value;
 
 public class CBI_RH_Processor2 implements ItemProcessor<Document, String> {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	Logger human_log = (Logger) LoggerFactory.getLogger("human_log");
-
-	// todo
-	@Value(value = "${xslt_file}")
-	private String xslt_file_name;
-
-	public void setXslt_file(String xslt_file) {
-		this.xslt_file_name = xslt_file;
-	}
-
+	
 	@Override
 	public String process(Document item) throws Exception {
-		// load the transformer using JAXP
-		// FIXME load once! caricare solo una volta possibilmente come bean
-		// configurato singleton
-		TransformerFactory factory = TransformerFactory.newInstance();
-		File xslt_file = new File(xslt_file_name);
-		InputStream stylesheet = new FileInputStream(xslt_file);
-		Transformer transformer = factory.newTransformer(new StreamSource(stylesheet));
+		
+		Transformer transformer = getTemplate().getCachedXSLT().newTransformer();
 
-		// now lets style the given document
+		// applica la trasformazione xslt
 		DocumentSource source = new DocumentSource(item);
 		DocumentResult result = new DocumentResult();
 		transformer.transform(source, result);
 
-		// return the transformed document
+		// restituisce il documento trasformato
 		Document transformedDoc = result.getDocument();
 
 		return transformedDoc.getRootElement().asXML();
 	}
+	
+	private XSLT_Templates template;
+
+	public XSLT_Templates getTemplate() {
+		return template;
+	}
+
+	public void setTemplate(XSLT_Templates templates) {
+		this.template = templates;
+	}
+
 
 }
---
+++ src/main/java/com/xmlconverter/XSLT_Templates.java
@@ -0,0 +1,40 @@
+package com.xmlconverter;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.Templates;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamSource;
+
+public class XSLT_Templates {
+
+	private Templates cachedXSLT;
+
+	public Templates getCachedXSLT() {
+		return cachedXSLT;
+	}
+
+	public XSLT_Templates(String xslt_file_name) {
+		// load the transformer using JAXP
+		TransformerFactory transFact = TransformerFactory.newInstance();
+		InputStream stylesheet;
+		try {
+			stylesheet = new FileInputStream(xslt_file_name);
+
+			Source xslt_source = new StreamSource(stylesheet);
+			cachedXSLT = transFact.newTemplates(xslt_source);
+
+		} catch (FileNotFoundException e) {
+			// logger.error
+			// throw exception
+		} catch (TransformerConfigurationException e) {
+			// logger.error
+			// throw exception
+		}
+	}
+
+}
